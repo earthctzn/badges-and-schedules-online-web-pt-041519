5b25b64f3899c1dac4f42a69b2f953756df49a1a
Caleb Aviles <earthctzn1@gmail.com>
2019-04-27 18:32:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,6 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(names)
-  rooms = [1..7]
   names.collect.with_index {|n, i| batch_badge_creator(n)}
   return "Hello #{n}! You'll be assigned to room #{i + 1}!"
 end
