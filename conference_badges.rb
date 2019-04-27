0beb4dbdb4b57cd942e29d6591d4139b1b134970
Caleb Aviles <earthctzn1@gmail.com>
2019-04-27 18:26:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,7 @@ end
 
 def assign_rooms(names)
   rooms = [1..7]
-  names.collect {|n| batch_badge_creator(n)}
-  return "Hello #{n}! You'll be assigned to room #{rooms}!"
+  names.collect.index {|n| batch_badge_creator(n)}
+  return "Hello #{n}! You'll be assigned to room #{rooms[i]!"
   rooms -= 1 
 end
